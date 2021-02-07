539b9194f277f9a02add4652e48caa3e66d2e919
Sid Roberts <sid@sidroberts.co.uk>
2021-02-07 09:37:16 +0900

    Added Terminal Style class.

---
+++ src/Console/Terminal/Style.php
@@ -0,0 +1,233 @@
+<?php
+
+namespace Centum\Console\Terminal;
+
+use InvalidArgumentException;
+
+class Style
+{
+    public function bold(string $text) : string
+    {
+        return $this->embed(
+            "\e[1m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function italics(string $text) : string
+    {
+        return $this->embed(
+            "\e[3m",
+            $text,
+            "\e[23m"
+        );
+    }
+
+    public function underline(string $text) : string
+    {
+        return $this->embed(
+            "\e[4m",
+            $text,
+            "\e[24m"
+        );
+    }
+
+    public function highlight(string $text) : string
+    {
+        return $this->embed(
+            "\e[7m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function dim(string $text) : string
+    {
+        return $this->embed(
+            "\e[2m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function blink(string $text) : string
+    {
+        return $this->embed(
+            "\e[5m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function reversed(string $text) : string
+    {
+        return $this->embed(
+            "\e[7m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+
+
+    public function textBlack(string $text) : string
+    {
+        return $this->embed(
+            "\e[30m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function textRed(string $text) : string
+    {
+        return $this->embed(
+            "\e[31m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function textGreen(string $text) : string
+    {
+        return $this->embed(
+            "\e[32m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function textYellow(string $text) : string
+    {
+        return $this->embed(
+            "\e[33m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function textBlue(string $text) : string
+    {
+        return $this->embed(
+            "\e[34m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function textMagenta(string $text) : string
+    {
+        return $this->embed(
+            "\e[35m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function textCyan(string $text) : string
+    {
+        return $this->embed(
+            "\e[36m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function textWhite(string $text) : string
+    {
+        return $this->embed(
+            "\e[37m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+
+
+    public function backgroundBlack(string $text) : string
+    {
+        return $this->embed(
+            "\e[40m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function backgroundRed(string $text) : string
+    {
+        return $this->embed(
+            "\e[41m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function backgroundGreen(string $text) : string
+    {
+        return $this->embed(
+            "\e[42m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function backgroundYellow(string $text) : string
+    {
+        return $this->embed(
+            "\e[43m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function backgroundBlue(string $text) : string
+    {
+        return $this->embed(
+            "\e[44m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function backgroundMagenta(string $text) : string
+    {
+        return $this->embed(
+            "\e[45m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function backgroundCyan(string $text) : string
+    {
+        return $this->embed(
+            "\e[46m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+    public function backgroundWhite(string $text) : string
+    {
+        return $this->embed(
+            "\e[47m",
+            $text,
+            "\e[0m"
+        );
+    }
+
+
+
+    public function reset() : string
+    {
+        return "\e[0m";
+    }
+
+
+
+    protected function embed(string $startCode, string $text, string $endCode) : string
+    {
+        return $startCode . str_replace("\e[0m", "\e[0m" . $startCode, $text) . $endCode;
+    }
+}
---
+++ tests/unit/Console/Terminal/StyleCest.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Centum\Tests\Console\Terminal;
+
+use Centum\Console\Terminal\Style;
+use Centum\Tests\UnitTester;
+use Codeception\Example;
+
+class StyleCest
+{
+    /**
+     * @dataProvider decorationProvider
+     */
+    public function decorations(UnitTester $I, Example $example)
+    {
+        $style = new Style();
+
+        $I->assertEquals(
+            $example["expected"],
+            call_user_func_array([$style, $example["method"]], ["Hello"])
+        );
+    }
+
+    public function decorationProvider() : array
+    {
+        return [
+            [
+                "expected" => "\e[1mHello\e[0m",
+                "method"   => "bold",
+            ],
+            [
+                "expected" => "\e[3mHello\e[23m",
+                "method"   => "italics",
+            ],
+            [
+                "expected" => "\e[4mHello\e[24m",
+                "method"   => "underline",
+            ],
+
+            [
+                "expected" => "\e[7mHello\e[0m",
+                "method"   => "highlight",
+            ],
+            [
+                "expected" => "\e[2mHello\e[0m",
+                "method"   => "dim",
+            ],
+
+            [
+                "expected" => "\e[5mHello\e[0m",
+                "method"   => "blink",
+            ],
+            [
+                "expected" => "\e[7mHello\e[0m",
+                "method"   => "reversed",
+            ],
+        ];
+    }
+
+
+
+    /**
+     * @dataProvider colorProvider
+     */
+    public function colors(UnitTester $I, Example $example)
+    {
+        $style = new Style();
+
+        $I->assertEquals(
+            $example["expected"],
+            call_user_func_array([$style, $example["method"]], ["Hello"])
+        );
+    }
+
+    public function colorProvider() : array
+    {
+        return [
+            [
+                "expected" => "\e[30mHello\e[0m",
+                "method"   => "textBlack",
+            ],
+            [
+                "expected" => "\e[31mHello\e[0m",
+                "method"   => "textRed",
+            ],
+            [
+                "expected" => "\e[32mHello\e[0m",
+                "method"   => "textGreen",
+            ],
+            [
+                "expected" => "\e[33mHello\e[0m",
+                "method"   => "textYellow",
+            ],
+            [
+                "expected" => "\e[34mHello\e[0m",
+                "method"   => "textBlue",
+            ],
+            [
+                "expected" => "\e[35mHello\e[0m",
+                "method"   => "textMagenta",
+            ],
+            [
+                "expected" => "\e[36mHello\e[0m",
+                "method"   => "textCyan",
+            ],
+            [
+                "expected" => "\e[37mHello\e[0m",
+                "method"   => "textWhite",
+            ],
+
+            [
+                "expected" => "\e[40mHello\e[0m",
+                "method"   => "backgroundBlack",
+            ],
+            [
+                "expected" => "\e[41mHello\e[0m",
+                "method"   => "backgroundRed",
+            ],
+            [
+                "expected" => "\e[42mHello\e[0m",
+                "method"   => "backgroundGreen",
+            ],
+            [
+                "expected" => "\e[43mHello\e[0m",
+                "method"   => "backgroundYellow",
+            ],
+            [
+                "expected" => "\e[44mHello\e[0m",
+                "method"   => "backgroundBlue",
+            ],
+            [
+                "expected" => "\e[45mHello\e[0m",
+                "method"   => "backgroundMagenta",
+            ],
+            [
+                "expected" => "\e[46mHello\e[0m",
+                "method"   => "backgroundCyan",
+            ],
+            [
+                "expected" => "\e[47mHello\e[0m",
+                "method"   => "backgroundWhite",
+            ],
+        ];
+    }
+
+
+
+    public function reset(UnitTester $I)
+    {
+        $style = new Style();
+
+        $I->assertEquals(
+            "\e[0m",
+            $style->reset()
+        );
+    }
+}
