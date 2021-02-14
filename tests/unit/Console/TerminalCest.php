9b549c777ecf146517d585ecbd5fdbdc708ecdfa
Sid Roberts <sid@sidroberts.co.uk>
2021-02-14 10:11:16 +0900

    Added Console\Terminal unit tests.

---
+++ tests/unit/Console/TerminalCest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Centum\Tests\Console;
+
+use Centum\Console\Terminal;
+use Centum\Tests\UnitTester;
+
+class TerminalCest
+{
+    public function testMultipleWritesToStdOut(UnitTester $I)
+    {
+        $argv = [
+            "cli.php",
+            "this:command:does:not:exist",
+        ];
+
+        $stdin = fopen("php://memory", "r");
+        $stdout = fopen("php://memory", "w");
+        $stderr = fopen("php://memory", "w");
+
+        $terminal = new Terminal($argv, $stdin, $stdout, $stderr);
+
+        $terminal->write("hello world");
+
+        $terminal->write(PHP_EOL);
+
+        $terminal->write("goodbye");
+
+        rewind($stdout);
+
+        $I->assertEquals(
+            "hello world" . PHP_EOL . "goodbye",
+            stream_get_contents($stdout)
+        );
+    }
+}
