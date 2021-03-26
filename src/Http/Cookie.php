d3390cee6bcb0e40aaef6f62f026f0a4be4a3755
Sid Roberts <sid@sidroberts.co.uk>
2021-03-26 23:42:53 +0900

    Added HTTP Cookie class.

--- src/Http/Cookie.php
+++ src/Http/Cookie.php
@@ -4,4 +4,37 @@ namespace Centum\Http;
 
 class Cookie
 {
+    protected string $name;
+    protected string $value;
+
+
+
+    public function __construct(string $name, string $value)
+    {
+        $this->name  = $name;
+        $this->value = $value;
+    }
+
+
+
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    public function getValue() : string
+    {
+        return $this->value;
+    }
+
+
+
+    public function __toString() : string
+    {
+        return sprintf(
+            "%s: %s\r\n",
+            $this->name,
+            $this->value
+        );
+    }
 }
