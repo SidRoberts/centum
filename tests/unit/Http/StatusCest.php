fd6e6061b3b2f8b5cd8c8318b57160411fb8612d
Sid Roberts <sid@sidroberts.co.uk>
2021-03-26 23:41:10 +0900

    Added HTTP Status Code class.

---
+++ tests/unit/Http/StatusCest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Http;
+
+use Centum\Http\Status;
+use OutOfRangeException;
+use Tests\UnitTester;
+
+class StatusCest
+{
+    public function test(UnitTester $I)
+    {
+        $status = new Status(404);
+
+        $I->assertEquals(
+            404,
+            $status->getCode()
+        );
+
+        $I->assertEquals(
+            "Not Found",
+            $status->getText()
+        );
+    }
+
+
+    public function testUnknownCode(UnitTester $I)
+    {
+        $status = new Status(499);
+
+        $I->assertEquals(
+            "Unknown",
+            $status->getText()
+        );
+    }
+
+    public function testInvalidCode(UnitTester $I)
+    {
+        $I->expectThrowable(
+            OutOfRangeException::class,
+            function () {
+                $status = new Status(999);
+            }
+        );
+    }
+}
