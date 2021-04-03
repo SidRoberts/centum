dc28c6f32a2efea25e0a3be4713d873d3b17107c
Sid Roberts <sid@sidroberts.co.uk>
2021-04-03 12:43:08 +0900

    Added VariableResponse to print variables.

---
+++ src/Http/Response/VariableResponse.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Centum\Http\Response;
+
+use Centum\Http\Header;
+use Centum\Http\Response;
+
+class VariableResponse extends Response
+{
+    public function __construct(mixed $variable)
+    {
+        $content = var_export($variable, true);
+
+        $headers = [
+            new Header("Content-Type", "text/plain"),
+        ];
+
+        parent::__construct($content, 200, $headers);
+    }
+}
---
+++ tests/unit/Http/Response/VariableResponseCest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Http\Response;
+
+use Centum\Http\Response\VariableResponse;
+use Codeception\Example;
+use stdClass;
+use Tests\Mvc\Filter\Doubler;
+use Tests\UnitTester;
+
+class VariableResponseCest
+{
+    /**
+     * @dataProvider provider
+     */
+    public function test(UnitTester $I, Example $example)
+    {
+        $response = new VariableResponse($example["variable"]);
+
+        $I->assertEquals(
+            $example["expected"],
+            $response->getContent()
+        );
+    }
+
+    public function provider(): array
+    {
+        return [
+            [
+                "variable" => "Hello Sid",
+                "expected" => "'Hello Sid'",
+            ],
+
+            [
+                "variable" => 0,
+                "expected" => "0",
+            ],
+
+            [
+                "variable" => true,
+                "expected" => "true",
+            ],
+
+            [
+                "variable" => [1,"a"],
+                "expected" => "array (" . PHP_EOL . "  0 => 1," . PHP_EOL . "  1 => 'a'," . PHP_EOL . ")",
+            ],
+
+            [
+                "variable" => new stdClass(),
+                "expected" => "(object) array(" . PHP_EOL . ")",
+            ],
+
+            [
+                "variable" => new Doubler(),
+                "expected" => Doubler::class . "::__set_state(array(" . PHP_EOL . "))",
+            ],
+        ];
+    }
+}
