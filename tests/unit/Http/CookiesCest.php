75991c001fcb20257bc04ccd004926aa8e1b927e
Sid Roberts <sid@sidroberts.co.uk>
2021-04-04 17:13:59 +0900

    Added tests for Http\Headers and Http\Cookies.

---
+++ tests/unit/Http/CookiesCest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Http;
+
+use Centum\Http\Cookie;
+use Centum\Http\Cookies;
+use Mockery;
+use Tests\UnitTester;
+
+class CookiesCest
+{
+    public function send(UnitTester $I): void
+    {
+        $cookie1 = Mockery::mock(Cookie::class);
+
+        $cookie1->expects()
+            ->getName()
+            ->andReturn("username");
+
+        $cookie1->expects()
+            ->send()
+            ->once();
+
+
+
+        $cookie2 = Mockery::mock(Cookie::class);
+
+        $cookie2->expects()
+            ->getName()
+            ->andReturn("language");
+
+        $cookie2->expects()
+            ->send()
+            ->once();
+
+
+
+        $cookie3 = Mockery::mock(Cookie::class);
+
+        $cookie3->expects()
+            ->getName()
+            ->andReturn("timezone");
+
+        $cookie3->expects()
+            ->send()
+            ->once();
+
+
+
+        $cookies = new Cookies();
+
+        $cookies->add($cookie1);
+        $cookies->add($cookie2);
+        $cookies->add($cookie3);
+
+        $cookies->send();
+    }
+
+    public function all(UnitTester $I): void
+    {
+        $cookie1 = new Cookie("username", "SidRoberts");
+        $cookie2 = new Cookie("language", "en");
+        $cookie3 = new Cookie("timezone", "Asia/Seoul");
+
+
+
+        $cookies = new Cookies();
+
+        $cookies->add($cookie1);
+        $cookies->add($cookie2);
+        $cookies->add($cookie3);
+
+
+
+        $I->assertEquals(
+            [
+                "username" => $cookie1,
+                "language" => $cookie2,
+                "timezone" => $cookie3,
+            ],
+            $cookies->all()
+        );
+    }
+}
---
+++ tests/unit/Http/HeadersCest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Http;
+
+use Centum\Http\Header;
+use Centum\Http\Headers;
+use Mockery;
+use Tests\UnitTester;
+
+class HeadersCest
+{
+    public function send(UnitTester $I): void
+    {
+        $header1 = Mockery::mock(Header::class);
+
+        $header1->expects()
+            ->getName()
+            ->andReturn("Content-Type");
+
+        $header1->expects()
+            ->send()
+            ->once();
+
+
+
+        $header2 = Mockery::mock(Header::class);
+
+        $header2->expects()
+            ->getName()
+            ->andReturn("Last-Modified");
+
+        $header2->expects()
+            ->send()
+            ->once();
+
+
+
+        $header3 = Mockery::mock(Header::class);
+
+        $header3->expects()
+            ->getName()
+            ->andReturn("Content-Encoding");
+
+        $header3->expects()
+            ->send()
+            ->once();
+
+
+
+        $headers = new Headers();
+
+        $headers->add($header1);
+        $headers->add($header2);
+        $headers->add($header3);
+
+        $headers->send();
+    }
+
+    public function all(UnitTester $I): void
+    {
+        $header1 = new Header("Content-Type", "text/plain");
+        $header2 = new Header("Last-Modified", "Sun, 04 Apr 2021 17:04:00 GMT");
+        $header3 = new Header("Content-Encoding", "gzip");
+
+
+
+        $headers = new Headers();
+
+        $headers->add($header1);
+        $headers->add($header2);
+        $headers->add($header3);
+
+
+
+        $I->assertEquals(
+            [
+                "Content-Type"     => $header1,
+                "Last-Modified"    => $header2,
+                "Content-Encoding" => $header3,
+            ],
+            $headers->all()
+        );
+    }
+}
