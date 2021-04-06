670393e54a9df459b9255bc1ba03ed7e95ff61ee
Sid Roberts <sid@sidroberts.co.uk>
2021-04-06 18:00:53 +0900

    Sessions made with `$_SESSION` are now started properly.

--- src/Http/Session/SessionGlobalVariableHandler.php
+++ src/Http/Session/SessionGlobalVariableHandler.php
@@ -18,16 +18,28 @@ class SessionGlobalVariableHandler implements HandlerInterface
 
     public function has(string $name): bool
     {
+        if (!$this->isActive()) {
+            $this->start();
+        }
+
         return isset($_SESSION[$name]);
     }
 
     public function get(string $name, mixed $defaultValue = null): mixed
     {
+        if (!$this->isActive()) {
+            $this->start();
+        }
+
         return $_SESSION[$name] ?? $defaultValue;
     }
 
     public function set(string $name, mixed $value): void
     {
+        if (!$this->isActive()) {
+            $this->start();
+        }
+
         /**
          * @var mixed
          */
@@ -36,16 +48,28 @@ class SessionGlobalVariableHandler implements HandlerInterface
 
     public function remove(string $name): void
     {
+        if (!$this->isActive()) {
+            $this->start();
+        }
+
         unset($_SESSION[$name]);
     }
 
     public function clear(): void
     {
+        if (!$this->isActive()) {
+            $this->start();
+        }
+
         $_SESSION = [];
     }
 
     public function all(): array
     {
+        if (!$this->isActive()) {
+            $this->start();
+        }
+
         return $_SESSION;
     }
 }
