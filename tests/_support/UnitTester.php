d2b598a7d68423119081a96d5b75b9d22e5edf03
Sid Roberts <sid@sidroberts.co.uk>
2021-04-12 08:02:37 +0900

    Fixed Psalm errors.

--- tests/_support/UnitTester.php
+++ tests/_support/UnitTester.php
@@ -6,19 +6,6 @@ use Codeception\Actor;
 use Throwable;
 
 /**
- * Inherited Methods
- *
- * @method void wantToTest($text)
- * @method void wantTo($text)
- * @method void execute($callable)
- * @method void expectTo($prediction)
- * @method void expect($prediction)
- * @method void amGoingTo($argumentation)
- * @method void am($role)
- * @method void lookForwardTo($achieveValue)
- * @method void comment($description)
- * @method void pause()
- *
  * @SuppressWarnings(PHPMD)
  */
 class UnitTester extends Actor
