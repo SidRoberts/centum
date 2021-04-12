da03a0e317490addcfcc5542c041013269de324c
Sid Roberts <sid@sidroberts.co.uk>
2021-04-12 12:02:47 +0900

    Added Alpha string filter.

---
+++ src/Filter/String/Alpha.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Centum\Filter\String;
+
+use Centum\Filter\FilterInterface;
+use InvalidArgumentException;
+
+class Alpha implements FilterInterface
+{
+    public function filter(mixed $value): mixed
+    {
+        if (!is_string($value)) {
+            throw new InvalidArgumentException("Value must be a string.");
+        }
+
+        return preg_replace("/[^A-Za-z]+/", "", $value);
+    }
+}
---
+++ tests/unit/Filter/String/AlphaCest.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Tests\Filter\String;
+
+use Centum\Filter\String\Alpha;
+use Codeception\Example;
+use InvalidArgumentException;
+use Tests\UnitTester;
+
+class AlphaCest
+{
+    /**
+     * @dataProvider provider
+     */
+    public function test(UnitTester $I, Example $example): void
+    {
+        $filter = new Alpha();
+
+        $actual = $filter->filter(
+            $example["value"]
+        );
+
+        $I->assertEquals(
+            $example["expected"],
+            $actual
+        );
+    }
+
+    public function provider(): array
+    {
+        return [
+            [
+                "value"    => "Sid Roberts",
+                "expected" => "SidRoberts",
+            ],
+
+            [
+                "value"    => "SidRoberts92",
+                "expected" => "SidRoberts",
+            ],
+
+            [
+                "value"    => "sid@sidroberts.co.uk",
+                "expected" => "sidsidrobertscouk",
+            ],
+
+            [
+                "value"    => "This is a sentence.",
+                "expected" => "Thisisasentence",
+            ],
+
+            [
+                "value"    => "https://github.com/SidRoberts/centum",
+                "expected" => "httpsgithubcomSidRobertscentum",
+            ],
+
+            [
+                "value"    => "그게 아니야",
+                "expected" => "",
+            ],
+        ];
+    }
+
+
+
+    /**
+     * @dataProvider providerException
+     */
+    public function exception(UnitTester $I, Example $example): void
+    {
+        $filter = new Alpha();
+
+        $I->expectThrowable(
+            new InvalidArgumentException("Value must be a string."),
+            function () use ($filter, $example): void {
+                $actual = $filter->filter(
+                    $example["value"]
+                );
+            }
+        );
+    }
+
+    public function providerException(): array
+    {
+        return [
+            [
+                "value" => true,
+            ],
+
+            [
+                "value" => 0,
+            ],
+
+            [
+                "value" => 123.456,
+            ],
+
+            [
+                "value" => ["1", 2, "three"],
+            ],
+
+            [
+                "value" => (object) ["1", 2, "three"],
+            ],
+        ];
+    }
+}
