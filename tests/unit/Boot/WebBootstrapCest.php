957a204c93e9a8761f8e0252e553eab8fbf0d8b6
Sid Roberts <sid@sidroberts.co.uk>
2021-04-17 18:35:14 +0900

    Added test for Web Bootstrap.

---
+++ tests/unit/Boot/WebBootstrapCest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Boot;
+
+use Centum\Boot\WebBootstrap;
+use Centum\Container\Container;
+use Centum\Http\Request;
+use Centum\Http\Response;
+use Centum\Mvc\Router;
+use Mockery;
+use Tests\UnitTester;
+
+class WebBootstrapCest
+{
+    public function test(UnitTester $I): void
+    {
+        $container = new Container();
+
+
+
+        $request = Mockery::mock(Request::class);
+
+        $container->set(Request::class, $request);
+
+
+
+        $response = Mockery::mock(Response::class);
+
+        $response->expects()
+            ->send();
+
+
+
+        $router = Mockery::mock(Router::class);
+
+        $router->expects()
+            ->handle()
+            ->with($request)
+            ->andReturn($response);
+
+        $container->set(Router::class, $router);
+
+
+
+        $bootstrap = new WebBootstrap();
+
+        $bootstrap->boot($container);
+    }
+}
