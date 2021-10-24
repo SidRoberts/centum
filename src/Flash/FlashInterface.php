bdfc81d9bec7e14b3354ada29e4408b7e9dfb1f1
Sid Roberts <sid@sidroberts.co.uk>
2021-10-25 05:34:47 +0900

    Corrected param names.

--- src/Flash/FlashInterface.php
+++ src/Flash/FlashInterface.php
@@ -4,13 +4,13 @@ namespace Centum\Flash;
 
 interface FlashInterface
 {
-    public function success(string $message): void;
+    public function success(string $text): void;
 
-    public function info(string $message): void;
+    public function info(string $text): void;
 
-    public function warning(string $message): void;
+    public function warning(string $text): void;
 
-    public function danger(string $message): void;
+    public function danger(string $text): void;
 
 
 
