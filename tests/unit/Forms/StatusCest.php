2d9e1dd4f26ed363b18f5ae26e848f634c2b97f4
Sid Roberts <sid@sidroberts.co.uk>
2022-09-13 23:02:54 +0900

    Added tests for Form\Status.

---
+++ tests/unit/Forms/StatusCest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Tests\Unit\Forms;
+
+use Centum\Forms\Status;
+use Codeception\Example;
+use Tests\UnitTester;
+
+class StatusCest
+{
+    /** @dataProvider providerIsValid */
+    public function testIsValid(UnitTester $I, Example $example): void
+    {
+        /** @var array<string, string[]> */
+        $messages = $example["messages"];
+
+        $status = new Status($messages);
+
+        /** @var bool */
+        $expected = $example["expected"];
+
+        $I->assertEquals(
+            $expected,
+            $status->isValid()
+        );
+    }
+
+    protected function providerIsValid(): array
+    {
+        return [
+            [
+                "messages" => [],
+                "expected" => true,
+            ],
+
+            [
+                "messages" => [
+                    "password" => ["Password is too short."],
+                ],
+                "expected" => false,
+            ],
+
+            [
+                "messages" => [
+                    "username" => ["Username is required."],
+                    "password" => ["Passwords don't match."],
+                ],
+                "expected" => false,
+            ],
+        ];
+    }
+
+
+
+    /** @dataProvider providerGetMessages */
+    public function testGetMessages(UnitTester $I, Example $example): void
+    {
+        /** @var array<string, string[]> */
+        $messages = $example["messages"];
+
+        $status = new Status($messages);
+
+        $I->assertEquals(
+            $messages,
+            $status->getMessages()
+        );
+    }
+
+    protected function providerGetMessages(): array
+    {
+        return [
+            [
+                "messages" => [],
+            ],
+
+            [
+                "messages" => [
+                    "password" => ["Password is too short."],
+                ],
+            ],
+
+            [
+                "messages" => [
+                    "username" => ["Username is required."],
+                    "password" => ["Passwords don't match."],
+                ],
+            ],
+        ];
+    }
+}
