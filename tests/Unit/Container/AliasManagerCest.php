0471aacb5ac101d50d9057fd3c37b57e00229ac6
Sid Roberts <sid@sidroberts.co.uk>
2023-09-14 16:10:10 +0900

    Added tests for `Container\AliasManager::getAll()`.

--- tests/Unit/Container/AliasManagerCest.php
+++ tests/Unit/Container/AliasManagerCest.php
@@ -3,9 +3,37 @@
 namespace Tests\Unit\Container;
 
 use Centum\Access\Access;
+use Centum\Console\Application;
+use Centum\Console\Terminal;
 use Centum\Container\AliasManager;
+use Centum\Cron\Cron;
+use Centum\Flash\Flash;
+use Centum\Flash\Formatter\HtmlFormatter;
+use Centum\Flash\Storage as FlashStorage;
+use Centum\Http\Csrf\Generator;
+use Centum\Http\Csrf\Storage;
+use Centum\Http\Csrf\Validator;
+use Centum\Http\Request;
+use Centum\Http\Session\GlobalSession;
 use Centum\Interfaces\Access\AccessInterface;
+use Centum\Interfaces\Console\ApplicationInterface;
 use Centum\Interfaces\Console\CommandInterface;
+use Centum\Interfaces\Console\TerminalInterface;
+use Centum\Interfaces\Cron\CronInterface;
+use Centum\Interfaces\Flash\FlashInterface;
+use Centum\Interfaces\Flash\FormatterInterface;
+use Centum\Interfaces\Flash\StorageInterface as FlashStorageInterface;
+use Centum\Interfaces\Http\Csrf\GeneratorInterface;
+use Centum\Interfaces\Http\Csrf\StorageInterface;
+use Centum\Interfaces\Http\Csrf\ValidatorInterface;
+use Centum\Interfaces\Http\RequestInterface;
+use Centum\Interfaces\Http\SessionInterface;
+use Centum\Interfaces\Queue\TaskRunnerInterface;
+use Centum\Interfaces\Router\RouterInterface;
+use Centum\Interfaces\Url\UrlInterface;
+use Centum\Queue\TaskRunner;
+use Centum\Router\Router;
+use Centum\Url\Url;
 use stdClass;
 use Tests\Support\Container\Alias\ACommand;
 use Tests\Support\UnitTester;
@@ -77,6 +105,34 @@ class AliasManagerCest
         $I->assertEquals(
             AccessInterface::class,
             $aliasManager->get(AccessInterface::class)
+        );
+    }
+
+
+
+    public function testGetAll(UnitTester $I): void
+    {
+        $aliasManager = new AliasManager();
+
+        $I->assertEquals(
+            [
+                AccessInterface::class       => Access::class,
+                ApplicationInterface::class  => Application::class,
+                TerminalInterface::class     => Terminal::class,
+                CronInterface::class         => Cron::class,
+                GeneratorInterface::class    => Generator::class,
+                StorageInterface::class      => Storage::class,
+                ValidatorInterface::class    => Validator::class,
+                FlashInterface::class        => Flash::class,
+                FormatterInterface::class    => HtmlFormatter::class,
+                FlashStorageInterface::class => FlashStorage::class,
+                RequestInterface::class      => Request::class,
+                SessionInterface::class      => GlobalSession::class,
+                RouterInterface::class       => Router::class,
+                UrlInterface::class          => Url::class,
+                TaskRunnerInterface::class   => TaskRunner::class,
+            ],
+            $aliasManager->getAll()
         );
     }
 }
